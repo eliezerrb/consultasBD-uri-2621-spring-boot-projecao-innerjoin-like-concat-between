be963245b0152dcfc6af257c0c6748fb33841eaf
eliezerrb <eliezerrezende@outlook.com>
2022-10-12 17:25:39 -0300

    URI 2621 Spring Boot SQL e JPQL com InnerJoin - like - comcat - between

--- src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
+++ src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import com.devsuperior.uri2621.dto.ProductMinDTO;
 import com.devsuperior.uri2621.entities.Product;
 import com.devsuperior.uri2621.projections.ProductMinProjection;
 
@@ -16,5 +17,12 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 			+ "WHERE products.amount BETWEEN :min AND :max "
 			+ "AND providers.name LIKE CONCAT(:BeginName, '%')")
 	List<ProductMinProjection> search1(Integer min, Integer max, String BeginName);
+	
+	// Consulta JPQL
+	@Query("SELECT new com.devsuperior.uri2621.dto.ProductMinDTO(obj.name) "
+			+ "FROM Product obj "
+			+ "WHERE obj.amount BETWEEN :min AND :max "
+			+ "AND obj.provider.name LIKE CONCAT(:BeginName, '%')")
+	List<ProductMinDTO> search2(Integer min, Integer max, String BeginName);
 
 }
